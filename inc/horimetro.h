2553f3b2201e5b49b0a1ed4c7950dbef8d677676
Giancarlo Bonvenuto <giancarlo.bonvenuto@gmail.com>
2024-10-26 17:27:18 -0300

    Código refatorado (ainda preciso comentar usando doxygen)

--- inc/horimetro.h
+++ inc/horimetro.h
@@ -7,9 +7,9 @@ struct {
   uint32_t ultimoPulso; // Ciclo em que o último pulso foi enviado
   uint32_t rpm; // RPM calculado
   uint32_t rpmMin; // RPM mínimo que o motor chega (abaixo disso o motor está desligado)
-  const struct gpio_dt_spec* dev; // Dev
   bool motorLigado;
-} sensor_hall;
+  const struct gpio_dt_spec* sensor_hall; // Dev
+} horimetro;
 
 // Essa função retorna quanto tempo faz desde o ultimo pulso
 int hall_init();
--- src/horimetro.c
+++ src/horimetro.c
@@ -6,6 +6,9 @@
 #include <zephyr/sys/time_units.h>
 #include <zephyr/timing/timing.h>
 
+// quantos milissegundos de gordurinha adicionamos ao timer
+#define GordurinhaTIMER 100
+
 LOG_MODULE_REGISTER(Horimetro);
 
 // Vou destacar tudo que tem a ver com o timer para que caso não funcione seja
@@ -15,73 +18,72 @@ LOG_MODULE_REGISTER(Horimetro);
 // executada, é porque o timer expirou e o motor está desligado)
 static struct k_timer my_timer;
 static void motor_desligado(struct k_timer *timer_id) {
-  sensor_hall.motorLigado = false;
+  horimetro.motorLigado = false;
   LOG_INF("Motor Desligado");
 };
 
 // === \Coisas to timer ===
 
 // Callback quando o pino vira HIGH
+// Pegamos o ciclo atual e a partir dele e do ultimoPulso conseguimos descobrir
+// o rpm. Então guardamos esse ciclo como o últimoPulso e definimos o motor como
+// ligado e reiniciamos o timer que definiria o motor como desligado
 static void hall_callback(const struct device *dev, struct gpio_callback *cb,
                           uint32_t pins) {
   uint32_t cycle = k_cycle_get_32();
-  sensor_hall.rpm =
-      60000 / k_cyc_to_ms_near32(abs(sensor_hall.ultimoPulso - cycle));
-  sensor_hall.ultimoPulso = cycle;
-  LOG_INF("RPM: %u", sensor_hall.rpm);
+  horimetro.rpm =
+      60000 / k_cyc_to_ms_near32(abs(horimetro.ultimoPulso - cycle));
+  horimetro.ultimoPulso = cycle;
+  LOG_INF("RPM: %u", horimetro.rpm);
 
   // Resetamos o timer que verifica se o motor está ligado
   // não precisamos veriricar se o motor está ligado se acabamos de receber
   // sinal dele
-  sensor_hall.motorLigado = true;
-  k_timer_start(
-      &my_timer,
-      K_MSEC((60000 / sensor_hall.rpmMin) + 100 // Uma gordurinha de 100ms
-             ),
-      K_NO_WAIT);
+  horimetro.motorLigado = true;
+  k_timer_start(&my_timer,
+                K_MSEC((60000 / horimetro.rpmMin) + GordurinhaTIMER),
+                K_NO_WAIT);
 }
 
 int hall_init() {
-  sensor_hall.ultimoPulso = -1; // Ciclo do último pulso
-  sensor_hall.rpm = 0;
-  sensor_hall.rpmMin = 1500; // E máximo por volta de 4200
+  horimetro.ultimoPulso = -1; // Ciclo do último pulso
+  horimetro.rpm = 0;
+  horimetro.rpmMin = 1500; // E máximo por volta de 4200
                              // Talvez possamos configurar isso aqui na
                              // Desvice tree? 🤔
-  sensor_hall.dev =
+  horimetro.sensor_hall =
       &(const struct gpio_dt_spec)GPIO_DT_SPEC_GET(DT_PATH(zephyr_user), gpios);
-  sensor_hall.motorLigado = false;
+  horimetro.motorLigado = false;
 
-  if (!gpio_is_ready_dt(sensor_hall.dev)) {
-    LOG_ERR("A porta %s não está pronta\n", sensor_hall.dev->port->name);
+  if (!gpio_is_ready_dt(horimetro.sensor_hall)) {
+    LOG_ERR("A porta %s não está pronta\n", horimetro.sensor_hall->port->name);
     return 0;
   }
 
   int err;
 
-  err = gpio_pin_configure_dt(sensor_hall.dev, GPIO_INPUT);
+  err = gpio_pin_configure_dt(horimetro.sensor_hall, GPIO_INPUT);
   if (err < 0) {
     LOG_ERR("Não foi possível configurar o pino do sensor (%d)", err);
     return err;
   }
 
   err =
-      gpio_pin_interrupt_configure_dt(sensor_hall.dev, GPIO_INT_EDGE_TO_ACTIVE);
+      gpio_pin_interrupt_configure_dt(horimetro.sensor_hall, GPIO_INT_EDGE_TO_ACTIVE);
 
   if (err < 0) {
     LOG_ERR("Não foi possível configurar o pino %s %d (Erro %d)",
-            sensor_hall.dev->port->name, sensor_hall.dev->pin, err);
+            horimetro.sensor_hall->port->name, horimetro.sensor_hall->pin, err);
   }
   return err;
 
-  gpio_init_callback(NULL, hall_callback, BIT(sensor_hall.dev->pin));
-  gpio_add_callback_dt(sensor_hall.dev, NULL);
+  gpio_init_callback(NULL, hall_callback, BIT(horimetro.sensor_hall->pin));
+  gpio_add_callback_dt(horimetro.sensor_hall, NULL);
 
   // Inicia o timer que verifica se o motor está ligado
   // __Esse timer não se reinicia sozinho__
   k_timer_init(&my_timer, motor_desligado, NULL);
-  k_timer_start(
-      &my_timer,
-      K_MSEC((60000 / sensor_hall.rpmMin) + 100 // Uma gordurinha de 100ms
-             ),
-      K_NO_WAIT);
+  k_timer_start(&my_timer,
+                K_MSEC((60000 / horimetro.rpmMin) + GordurinhaTIMER),
+                K_NO_WAIT);
 }
